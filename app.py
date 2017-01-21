19efdc1b27558ceceedf0ef9cbd75ec5be704262
Benjamin Efron <Benjamin Efron>
2017-01-21 04:07:14 -0500

    updated newUser and finished basic setUserData

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import bleach
 import datetime
 
 import pymongo
-# from pymongo import MongoClient
-# from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
 client = pymongo.MongoClient("mongodb://venerdm:Rose!2@drankonline-shard-00-00-ofd8a.mongodb.net:27017,drankonline-shard-00-01-ofd8a.mongodb.net:27017,drankonline-shard-00-02-ofd8a.mongodb.net:27017/admin?ssl=true&replicaSet=DrankOnline-shard-0&authSource=admin")
@@ -27,6 +25,8 @@ def emptyPersonObject():
     }
 
     return person
+
+#TODO Update all database interactions to use transactions, so if some operation fails the entire thing aborts
 
 @app.route('/')
 def main():
@@ -54,29 +54,47 @@ def createAccount():
     # create a new person to associate with our new user
     newPerson = emptyPersonObject()
     result = personTable.insert_one(newPerson)
+    if not result.acknowledged:
+        return jsonify({'failure' : 'data insertion failure'})
 
     newUser = {'email' : newUserEmail, 'password' : newUserPassword, 'personID' : result.inserted_id}
     result = userTable.insert_one(newUser)
     if not result.acknowledged:
         return jsonify({'failure' : 'data insertion failure'})
     
+    #TODO update this to also return the json token 
     return jsonify({'success' : 'successfully added new user'})
 
-# @app.rought('/setUserData', methods=['POST'])
-# def setUserData():
-#     if request.method != 'POST':
-#         return jsonify({'failure' : 'incorrect request format'})
+@app.rought('/setUserData', methods=['POST'])
+def setUserData():
+    if request.method != 'POST':
+        return jsonify({'failure' : 'incorrect request format'})
     
-#     fieldsTracked = ['age', 'name', 'sex', 'height', 'weight']
+    #some setup
+    fieldsTracked = ['age', 'name', 'sex', 'height', 'weight']
+    data = request.get_json()
 
-#     data = request.get_json()
-#     keyList = []
+    userTable = db.user
+    personTable = db.person
 
-#     #iterate through the JSON that was passed, do a sanity check on the keys, and then insert the data
-#     for key in data.keys():
-#         if key in fieldsTracked:
-#             keyList.append(key)
-    
+    #TODO update this to grab json token from request header
+    #grab email from the sent data and get the personID associated with this email
+    if 'email' not in data.keys():
+        return jsonify({'failure' : 'user\'s email was not included with this request'})
+
+    userEmail = bleach.clean(data['email'])
+    userObject = userTable.find_one({'email' : userEmail})
+    personID = userObject['personID']
+
+    #now use the person object to update all of the fields that were sent
+    for key in data.keys():
+        if key in fieldsTracked:
+            result = personTable.find_one_and_update({'_id' : personID}, {'$set':{key : bleach.clean(data[key])}})
+            
+            if not result.acknowledged:
+                return jsonify({'failure' : 'data update failure'})
+        
+    return jsonify({'success' : 'successfully added new user'})    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
