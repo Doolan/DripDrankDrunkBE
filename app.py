734dcd3a306c5cffd9a28b4d42ec8a8c962b0c77
dylanvener <dylanvener1@gmail.com>
2017-01-22 02:55:16 -0500

    :a

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def getTonight(nightObjects, currentTime = None):
     if currentTime is None:
         currentTime = datetime.datetime.today()
     for night in nightObjects:
-        if currentTime >= datetime.datetime.utcfromtimestamp(night['dateStart']) and currentTime <= datetime.datetime.utcfromtimestamp(night['dateEnd']);
+        if currentTime >= datetime.datetime.utcfromtimestamp(night['dateStart']) and currentTime <= datetime.datetime.utcfromtimestamp(night['dateEnd']):
             return night
     return None
 
